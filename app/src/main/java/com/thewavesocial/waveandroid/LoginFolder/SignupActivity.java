cece37a1ff408464037dcf2b9563e67572a33ab4
Kaushik Mahorker <kmahorker@gmail.com>
2017-03-08 17:10:26 -0800

    Added SignUpActivity to Manifest, and added temporary workaround for runtime error when passing intent to SignUpActivity from LoginActivity

--- app/src/main/java/com/thewavesocial/waveandroid/LoginFolder/SignupActivity.java
+++ app/src/main/java/com/thewavesocial/waveandroid/LoginFolder/SignupActivity.java
@@ -40,10 +40,12 @@ public class SignupActivity extends FragmentActivity
         setContentView(R.layout.signup_viewpager_layout);
 
         Intent intent = getIntent();
-        userID = Long.parseLong(intent.getExtras().getString("userIDLong"));
-        name = intent.getExtras().getString("userName");
-        email = intent.getExtras().getString("userEmail");
-        gender = intent.getExtras().getString("userGender");
+        if(intent.getExtras() != null) { //TODO 3/8/2017 Only Get Extras if Login with Facebook
+            userID = Long.parseLong(intent.getExtras().getString("userIDLong"));
+            name = intent.getExtras().getString("userName");
+            email = intent.getExtras().getString("userEmail");
+            gender = intent.getExtras().getString("userGender");
+        }
 
         try
         {
