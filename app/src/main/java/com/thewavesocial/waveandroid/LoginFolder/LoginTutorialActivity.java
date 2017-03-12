5bb384a3a53d8348225c867946f0d1cefecf05a4
Kaushik Mahorker <kmahorker@gmail.com>
2017-03-12 16:11:20 -0700

    Changed radius of buttons. Added Activity for new login design. Finished Login Activity Layout. TODO: Login Fragment layout and inflation.

---
+++ app/src/main/java/com/thewavesocial/waveandroid/LoginFolder/LoginTutorialActivity.java
@@ -0,0 +1,15 @@
+package com.thewavesocial.waveandroid.LoginFolder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.thewavesocial.waveandroid.R;
+
+public class LoginTutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_tutorial);
+    }
+}
