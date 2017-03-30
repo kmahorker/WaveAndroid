0cde551936a555ab560acc0b6fa363f33639d56f
Kaushik Mahorker <kmahorker@gmail.com>
2017-03-30 15:25:58 -0700

    Added all UI elements, changed gradle dependency to add custom two thumb range bar

---
+++ app/src/main/java/com/thewavesocial/waveandroid/HostFolder/CreateAnEventActivity.java
@@ -0,0 +1,15 @@
+package com.thewavesocial.waveandroid.HostFolder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.thewavesocial.waveandroid.R;
+
+public class CreateAnEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_an_event);
+    }
+}
