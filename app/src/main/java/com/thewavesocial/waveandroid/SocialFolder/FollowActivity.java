5bf8a9d1da61915cfac744d20ca0484a1342fad5
Wei Tung Chen <lleeoochen@gmail.com>
2017-04-19 02:31:39 -0700

    Finished search functionality, hide keyboard on default, and removed options for the leftover Following/Followers activity

--- app/src/main/java/com/thewavesocial/waveandroid/SocialFolder/FollowActivity.java
+++ app/src/main/java/com/thewavesocial/waveandroid/SocialFolder/FollowActivity.java
@@ -32,14 +32,10 @@ import java.util.List;
 
 public class FollowActivity extends AppCompatActivity {
     public static final String TAG = FollowActivity.class.getName();
-
     public static final String FOLLOW_POPUP_TYPE_ARG = "FOLLOW_POPUP_TYPE_ARG";
-
     private Activity followActivity = this;
     private UserProfileFragment.PopupPage pageType;
-
     private TextView title;
-
     private SearchView searchField;
     private ListView followUsersList;
 
@@ -57,30 +53,31 @@ public class FollowActivity extends AppCompatActivity {
     }
 
     public void createViews() {
+        followUsersList = (ListView) findViewById(R.id.lv_follow_follows_list);
+
+        List<User> follows = new ArrayList<>();
+        if (pageType == UserProfileFragment.PopupPage.FOLLOWERS) //Changed to getUsersListObjects()
+            follows = CurrentUser.getUsersListObjects( CurrentUser.theUser.getFollowers() );
+        else if (pageType == UserProfileFragment.PopupPage.FOLLOWING)
+            follows = CurrentUser.getUsersListObjects( CurrentUser.theUser.getFollowing() );
+        followUsersList.setAdapter(new SearchPeopleCustomAdapter(this, follows));
+
+        final List<User> finalFollows = follows;
         searchField = (SearchView) findViewById(R.id.sf_follow_search_field);
         searchField.setQueryHint(getString(R.string.search));
         searchField.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                //TODO, no functionality due to unknown search requirements
+                followUsersList.setAdapter(new SearchPeopleCustomAdapter(followActivity, UtilityClass.search(finalFollows, query)));
                 return true;
             }
 
             @Override
-            public boolean onQueryTextChange(String newText) {
+            public boolean onQueryTextChange(String query) {
+                followUsersList.setAdapter(new SearchPeopleCustomAdapter(followActivity, UtilityClass.search(finalFollows, query)));
                 return true;
             }
         });
-
-        followUsersList = (ListView) findViewById(R.id.lv_follow_follows_list);
-        List<User> follows = new ArrayList<>();
-
-        if (pageType == UserProfileFragment.PopupPage.FOLLOWERS) //Changed to getUsersListObjects()
-            follows = CurrentUser.getUsersListObjects( CurrentUser.theUser.getFollowers() );
-        else if (pageType == UserProfileFragment.PopupPage.FOLLOWING)
-            follows = CurrentUser.getUsersListObjects( CurrentUser.theUser.getFollowing() );
-
-        followUsersList.setAdapter(new SearchPeopleCustomAdapter(this, follows));
     }
 
     public void setupFollowActionbar() {
@@ -103,62 +100,4 @@ public class FollowActivity extends AppCompatActivity {
         }
     }
 
-//    private class FollowersAdapter extends ArrayAdapter<User> {
-//
-//        public FollowersAdapter(Context context, int textViewResourceId, List<User> follows) {
-//            super(context, textViewResourceId, follows);
-//        }
-//
-//        @Override
-//        public View getView(final int position, View convertView, ViewGroup parent) {
-//            View v = convertView;
-//            if (v == null) {
-//                LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//                v = vi.inflate(R.layout.follow_user_row, null);
-//                v.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        UtilityClass.hideKeyboard(followActivity);
-//                    }
-//                });
-//            }
-//            final User user = getItem(position);
-//            if (user != null) {
-//
-//                final ImageView userImage = (ImageView) v.findViewById(R.id.iv_follow_row_user_image);
-//                if (userImage != null) {
-//                    BitmapDrawable pic = user.getProfilePic();
-//                    if (pic != null) {
-//                        userImage.setImageDrawable(UtilityClass.toRoundImage(getResources(), pic.getBitmap()));
-//                    }
-//                }
-//
-//                final TextView name = (TextView) v.findViewById(R.id.iv_follow_row_user_name);
-//                if (name != null) {
-//                    if (user.getFullName() == null) {
-//                        //TODO
-//                        name.setText("Unknown Name");
-//                    } else {
-//                        name.setText(user.getFullName());
-//                    }
-//                }
-//
-//                final Button followed = (Button) v.findViewById(R.id.iv_follow_row_user_followed);
-//                if (followed != null) {
-//                    if (CurrentUser.theUser.getFollowing().contains(user.getUserID())) {
-//                        followed.setText(getResources().getString(R.string.follow_row_following));
-//                        followed.setTextColor(getResources().getColor(R.color.white_solid));
-//                        followed.setBackgroundResource(R.drawable.rounded_pink_button);
-//                    } else {
-//                        followed.setText(getResources().getString(R.string.follow_row_follow));
-//                        followed.setTextColor(getResources().getColor(R.color.pink_solid));
-//                        followed.setBackgroundResource(R.drawable.rounded_pink_button_stroke);
-//                    }
-//
-//                }
-//
-//            }
-//            return v;
-//        }
-//    }
 }
