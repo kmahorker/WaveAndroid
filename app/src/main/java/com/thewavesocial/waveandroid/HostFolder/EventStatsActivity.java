190baa6442861f0a4b3dac441d0d077c3e570bf7
Wei Tung Chen <lleeoochen@gmail.com>
2017-05-27 15:33:44 -0700

    Update Attending users and host name with server in EventStats

--- app/src/main/java/com/thewavesocial/waveandroid/HostFolder/EventStatsActivity.java
+++ app/src/main/java/com/thewavesocial/waveandroid/HostFolder/EventStatsActivity.java
@@ -30,6 +30,7 @@ import com.thewavesocial.waveandroid.R;
 import com.thewavesocial.waveandroid.UtilityClass;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class EventStatsActivity extends AppCompatActivity implements OnMapReadyCallback{
@@ -51,16 +52,6 @@ public class EventStatsActivity extends AppCompatActivity implements OnMapReadyC
         mainActivity = this;
         Intent intent = getIntent();
         party = intent.getExtras().getParcelable("partyObject");
-//        CurrentUser.server_getPartyObject(intent.getExtras().getString("partyIDLong"), new OnResultReadyListener<Party>() {
-//            @Override
-//            public void onResultReady(Party result) {
-//                if ( result != null ) {
-//                    party = result;
-//                    setupReferences();
-//                    setupActionbar();
-//                }
-//            }
-//        });
 
         setupActionbar();
         setupReferences();
@@ -90,7 +81,6 @@ public class EventStatsActivity extends AppCompatActivity implements OnMapReadyC
 
 
     private void setupPartyInfos() {
-        host = party.getHostName();
         loc = party.getMapAddress().getAddress_string();
         date = UtilityClass.dateToString(party.getStartingDateTime()) + " - " +
                 UtilityClass.dateToString(party.getEndingDateTime());
@@ -125,11 +115,24 @@ public class EventStatsActivity extends AppCompatActivity implements OnMapReadyC
         qrCodeView.setImageDrawable(getDrawable(R.drawable.sample_qrcode));
 
         final List<User> sample = new ArrayList();
+        CurrentUser.server_getUsersOfEvent(party.getPartyID(), new OnResultReadyListener<HashMap<String, ArrayList<User>>>() {
+            @Override
+            public void onResultReady(HashMap<String, ArrayList<User>> result) {
+                if ( result != null ) {
+                    hostView.setText(result.get("hosting").get(0).getFullName());
+
+                    sample.addAll(result.get("attending"));
+                    LinearLayoutManager layoutManagerAttendees = new LinearLayoutManager(mainActivity, LinearLayoutManager.HORIZONTAL, false);
+                    attendingFriends.setLayoutManager(layoutManagerAttendees);
+                    attendingFriends.setFocusable(false);
+                    attendingFriends.setAdapter(new PartyAttendeesCustomAdapter(mainActivity, sample));
+                }
+            }
+        });
         CurrentUser.server_getUsersListObjects(party.getAttendingUsers(), new OnResultReadyListener<List<User>>() {
             @Override
             public void onResultReady(List<User> result) {
                 if ( result != null ) {
-                    sample.addAll(result);
                     LinearLayoutManager layoutManagerAttendees = new LinearLayoutManager(mainActivity, LinearLayoutManager.HORIZONTAL, false);
                     attendingFriends.setLayoutManager(layoutManagerAttendees);
                     attendingFriends.setFocusable(false);
