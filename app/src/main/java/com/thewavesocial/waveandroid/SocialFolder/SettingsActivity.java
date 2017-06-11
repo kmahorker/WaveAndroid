33105d261f0cd43450454169afb2980a098b2b18
Wei Tung Chen <lleeoochen@gmail.com>
2017-06-11 14:40:10 -0700

    Added log out functionality in settings page

--- app/src/main/java/com/thewavesocial/waveandroid/SocialFolder/SettingsActivity.java
+++ app/src/main/java/com/thewavesocial/waveandroid/SocialFolder/SettingsActivity.java
@@ -1,10 +1,17 @@
 package com.thewavesocial.waveandroid.SocialFolder;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.facebook.login.LoginManager;
+import com.thewavesocial.waveandroid.DatabaseObjects.DatabaseAccess;
+import com.thewavesocial.waveandroid.HomeSwipeActivity;
+import com.thewavesocial.waveandroid.LoginFolder.LoginTutorialActivity;
 import com.thewavesocial.waveandroid.R;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -55,7 +62,32 @@ public class SettingsActivity extends AppCompatActivity {
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(mainActivity);
+                dialog.setTitle("Logging out");
+                dialog.setMessage("Are you sure you want to log out of current session?");
+                dialog.setCancelable(false);
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        LoginManager.getInstance().logOut();
+                        DatabaseAccess.saveTokentoLocal(mainActivity, "", "");
 
+                        //Clear all activities and go to LoginTutorial Page
+                        Intent intent = new Intent(getApplicationContext(), LoginTutorialActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
             }
         });
 
