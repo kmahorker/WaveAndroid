06a823da1cd03eac812df8624177b6ec0a030d62
Wei Tung Chen <lleeoochen@gmail.com>
2017-06-25 22:02:43 -0700

    Added bitmap downsize function in UtilityClass

--- app/src/main/java/com/thewavesocial/waveandroid/UtilityClass.java
+++ app/src/main/java/com/thewavesocial/waveandroid/UtilityClass.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Address;
 import android.location.Geocoder;
 import android.support.annotation.IdRes;
@@ -27,6 +28,7 @@ import java.io.IOException;
 
 import com.thewavesocial.waveandroid.BusinessObjects.User;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -146,6 +148,42 @@ public final class UtilityClass {
         RoundedBitmapDrawable dr = RoundedBitmapDrawableFactory.create(res, bitmap);
         dr.setCircular(true);
         return dr;
+    }
+
+    public static Bitmap downsizeBitmapStream(InputStream stream, int reqWidth, int reqHeight) {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(stream, null, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeStream(stream, null, options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
     /**
@@ -416,5 +454,13 @@ public final class UtilityClass {
             }
         }
         return duplicates;
+    }
+
+    private static class BitmapDimension {
+        public int width, height;
+        public BitmapDimension(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
     }
 }
